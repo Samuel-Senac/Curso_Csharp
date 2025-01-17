ab377930ac3e2c25a52fd639d98ea9f8bedc3c4c
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-16 21:23:00 -0300

    Adicionando Recuperação

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -4,13 +4,19 @@
     {
         static void Main(string[] args)
         {
-            double nota_final = 50;
+            double nota_final = 30;
             string resultado;
        
 
             if (nota_final >= 60)
             {
                 resultado = "Aprovado";
+            }
+
+
+            else if (nota_final >= 40)
+            {
+                resultado = "Recuperação";
             }
 
 
