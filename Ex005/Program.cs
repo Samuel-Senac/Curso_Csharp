28e752a94f68cd1293942e0a45afbf5e31ca7e5e
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-21 19:32:49 -0300

    Exercício de Teste do Triângulo

---
+++ Ex005/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.WriteLine("Digite a nota do primeiro bimestre");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do segundo bimestre:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            num3 = (num1 + num2) / 2;
+
+            Console.WriteLine("A média de ({0} + {1}) / 2 = {2}", num1, num2, num3);
+
+        }
+    }
+}
