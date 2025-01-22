d812fc0a65d945b5e28a0c07fdedc3ca1426a082
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-21 22:10:49 -0300

    exercicio do salario

---
+++ Ex0010/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex0010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual o valor do salário recebido: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            double aumento = 0;
+            double novosalario = 0;
+
+            if (salario > 2500)
+            {
+                aumento = salario * 0.05;
+            }
+            else if (salario > 1250)
+            {
+                aumento = salario * 0.10;
+            }
+            else
+            {
+                aumento = salario * 0.15;
+            }
+            novosalario = salario + aumento;
+
+            Console.WriteLine($"O valor do aumento é R${aumento:0}");
+            
+        }
+    }
+}
---
+++ Ex0011/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex0011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double casa, salario, anos;
+
+            Console.WriteLine("Qual o valor da casa: ");
+            casa = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual a sua renda atual: ");
+            salario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Em quantos anos será quitada a sua divída referente a casa: ");
+            anos = Convert.ToDouble(Console.ReadLine());
+
+            
+        }
+    }
+}
