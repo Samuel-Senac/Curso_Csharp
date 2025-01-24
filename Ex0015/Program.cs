f2be87cf6b390701e65364f9ca5f853804bc84d5
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-23 21:36:20 -0300

    Lição do gênero

---
+++ Ex0015/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Ex0015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genero;
+
+            Console.Write("Digite o gênero: ");
+            genero = Console.ReadLine().ToUpper();
+
+            while(genero != "F" && genero != "M")
+            {
+                Console.Write("Digite o gênero: ");
+                genero = Console.ReadLine();
+            }
+
+            Console.WriteLine("Bem-Vindos e Bem-Vindas ao curso de C#");
+
+        }
+    }
+}
