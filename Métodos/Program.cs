bb0d9e13dbb4e2134ee1fb8ce64f3f3ced93f659
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-30 20:53:56 -0300

    métodos

---
+++ Métodos/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Métodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Saudacao("Samuel Ferreira");
+            Soma(10, 20);
+        }
+        static void Saudacao(string nome)
+        {
+            Console.WriteLine($"Olá {nome}, seja bem-vindo!");
+        }
+
+        static void Soma(int a, int b)
+        {
+            int resultado = a + b;
+            Console.WriteLine($"A soma de {a} + {b} é igual a {resultado}");
+            Console.WriteLine("A soma de {0} + {1} é igual a {3}.", a, b, resultado);
+            Console.WriteLine("A soma de " + a + " + " + b + " é igual a " + resultado + ".");
+        }
+    }
+}
