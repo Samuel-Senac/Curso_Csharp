0d78e4eb6b56d6b1f2f31cfd2ffb2fac331aa800
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-30 21:10:10 -0300

    terreno

---
+++ Calculo Área do Triângulo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculo_Área_do_Triângulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
