9d5c3617b4aa8ab5b63b306083e194019967ae5f
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-30 21:36:46 -0300

    cálculo de base do triângulo

--- Calculo Área do Triângulo/Program.cs
+++ Calculo Área do Triângulo/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("=============================");
+            Console.WriteLine("Cálculo_de_Área_do_Trinângulo");
+            Console.WriteLine("=============================");
+            Console.WriteLine();
+
+            Console.WriteLine("Digite o valor da base do triângulo");
+            double ba = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a altura do triângulo");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            BaseAltura(ba, h);
+
+        }
+
+        static void BaseAltura(double b, double a)
+        {
+            double x = (b * a) / 2;
+            Console.WriteLine($"A área do triângulo de {b} * {a} / 2 é de {x}.");
         }
     }
 }
