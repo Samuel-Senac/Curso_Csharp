dcee01e68e7068601a1f9d33745cceab02ee95c3
SAMUEL FERREIRA CARVALHO MARIANO <samuel.fcmariano@senacsp.edu.br>
2025-01-30 21:49:03 -0300

    Escrever

---
+++ Escrever/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Escrever
+{
+    internal class Program
+    {
+        static void Escrever(string texto)
+        {
+            int tam = texto.Length + 4;
+            string linha = new('*', tam);
+            string textoCentralizado = texto.PadLeft(tam - 2).PadRight(tam - 2);
+            Console.WriteLine(linha);
+            Console.WriteLine(textoCentralizado);
+            Console.WriteLine(linha);
+        }
+
+        static void Main(string[] args)
+        {
+            Escrever("Samuel Ferreira Carvalho Mariano");
+            Console.WriteLine();
+            Escrever("Curso#");
+            Console.WriteLine();
+            Escrever("Senac - L13");
+            Console.WriteLine();
+        }
+    }
+}
